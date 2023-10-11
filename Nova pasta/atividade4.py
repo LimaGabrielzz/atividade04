f563e9ae7a2af4ea7b4e4cceee79a3c6a29e9c18
LimaGabrielzz <gabriel.lima318@aluno.ce.gov.br>
2023-10-11 10:58:24 -0300

    first commit

---
+++ Nova pasta/atividade4.py
@@ -0,0 +1,8 @@
+# Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+import time
+numero = float(input("digite um número: "))
+antecessor = numero - 1
+print(antecessor)
+time.sleep(1)
+sucessor = numero + 1
+print(sucessor)
